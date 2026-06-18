54bed49213765fa36b61ba2589990077d21eab32
praveen-503 <luckyindrakanti@gmail.com>
2026-06-18 18:59:35 +0530

    CORS Policy added with front-end app

--- HotelStay.Api/Program.cs
+++ HotelStay.Api/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://localhost:4200")
+        policy.WithOrigins("http://localhost:4200", "https://hotelstay-booking-portal.vercel.app")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
